f51580cd316349cccb379f51b54debbd03c12b62
Thomas Norris <tom.norris135@gmail.com>
2018-08-13 10:34:27 -0400

    add node-rest-client; start program.js

---
+++ program.js
@@ -0,0 +1,27 @@
+(function() {
+    var RestClient = require('node-rest-client').Client;
+    var _client = new RestClient();
+
+    // --Add key
+    const KEY = '';
+    var testRequest = new Request('test_event', 'test value 1', 'test value 1', 'test value 3');
+
+    // _client.post(testRequest.Url, testRequest.Args, (data, res) => {
+
+    // });
+
+    function Request(eventName, val1, val2, val3) {
+        this.EventName = eventName;
+        this.Url = 'https://maker.ifttt.com/trigger/' + eventName + '/with/key/' + KEY;
+        this.Args = {
+            data: {
+                'value1': val1,
+                'value2': val2,
+                'value3': val3
+            },
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+    }
+})();
