bc9de4c5be4c825bce6b628b56e79100d8bb0cac
Thomas Norris <tom.norris135@gmail.com>
2018-08-13 11:24:56 -0400

    added request.js object; moved stuff to it

--- program.js
+++ program.js
@@ -1,28 +1,7 @@
 global.requireLocal = require('local-modules.js').GetModule;
 
 (function() {
-    var RestClient = require('node-rest-client').Client;
-    var _client = new RestClient();
+    var Request = requireLocal('request.js').Request;
 
-    // --Add key
-    const KEY = requireLocal('ifttt-key.js').GetKey();
-    var testRequest = new Request('test_event', 'test value 1', 'test value 1', 'test value 3');
-    // _client.post(testRequest.Url, testRequest.Args, (data, res) => {
-
-    // });
-
-    function Request(eventName, val1, val2, val3) {
-        this.EventName = eventName;
-        this.Url = 'https://maker.ifttt.com/trigger/' + eventName + '/with/key/' + KEY;
-        this.Args = {
-            data: {
-                'value1': val1,
-                'value2': val2,
-                'value3': val3
-            },
-            headers: {
-                "Content-Type": "application/json"
-            }
-        };
-    }
+    new Request('test_event', 'new val 1', 'something', 'hi mom').Post();
 })();
