e73b91803ce46b5749513c97801fadd48bc66e7a
Thomas Norris <tom.norris135@gmail.com>
2018-08-17 08:49:44 -0400

    Update program.js

--- bin/program.js
+++ bin/program.js
@@ -2,11 +2,11 @@ global.requireLocal = require('local-modules.js').GetModule;
 
 (function() {
     var Request = requireLocal('request.js').Request;
-    // 'test_event' just sends an email with the params
-    var req = new Request('test_event', 'new val 1', 'something', 'hi mom');
-
+    // 'test_event' is the IFTTT event name that will be triggered, the other parameters are optional
+    var req = new Request('test_event', 'dummyVal1', 'dummyVal2', 'dummyVal3');
+    
+    // Send the request and handle the response
     req.Send((data, res) => {
         console.log(data, res);
     });
-
 })();
